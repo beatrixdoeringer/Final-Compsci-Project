e1d3d3332c2dd830ad8fee5b9b6ce0d6d76231f5
HBaconburger <hbreed120@icloud.com>
2025-05-29 13:50:31 -0400

    juytfjytdf

---
+++ Obstacle.py
@@ -0,0 +1,11 @@
+import math
+import pygame
+import sys
+import pygame.locals
+import random_move_dune
+class Enemy:
+    def __init__(self,range_mot:int,horizontal_pos:int):
+        self.horizontal_pos=horizontal_pos
+        self.range_mot=range_mot
+    def destroyplayer():
+        
